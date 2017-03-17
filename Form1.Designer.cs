3abe594ea782307b1f8decb354247664122d3ef6
Palmanius <palmanius@gmail.com>
2017-03-17 12:27:14 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class CTM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.termsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sentancesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.connectDataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Source = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Target = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.termDataGridView = new System.Windows.Forms.DataGridView();
+            this.ID2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Term = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sentanceDataGridView = new System.Windows.Forms.DataGridView();
+            this.ID3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Veta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pojem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.connectDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.termDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sentanceDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(345, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsStripMenuItem,
+            this.importToolStripMenuItem,
+            this.connectToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsStripMenuItem
+            // 
+            this.saveAsStripMenuItem.Name = "saveAsStripMenuItem";
+            this.saveAsStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsStripMenuItem.Text = "Save as";
+            this.saveAsStripMenuItem.Click += new System.EventHandler(this.saveAsStripMenuItem_Click);
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.termsToolStripMenuItem,
+            this.sentancesToolStripMenuItem});
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // termsToolStripMenuItem
+            // 
+            this.termsToolStripMenuItem.Name = "termsToolStripMenuItem";
+            this.termsToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.termsToolStripMenuItem.Text = "Terms";
+            this.termsToolStripMenuItem.Click += new System.EventHandler(this.termsToolStripMenuItem_Click);
+            // 
+            // sentancesToolStripMenuItem
+            // 
+            this.sentancesToolStripMenuItem.Name = "sentancesToolStripMenuItem";
+            this.sentancesToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.sentancesToolStripMenuItem.Text = "Sentances";
+            this.sentancesToolStripMenuItem.Click += new System.EventHandler(this.sentancesToolStripMenuItem_Click);
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Enabled = false;
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // connectDataGridView
+            // 
+            this.connectDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.connectDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Source,
+            this.Target});
+            this.connectDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.connectDataGridView.Location = new System.Drawing.Point(0, 24);
+            this.connectDataGridView.Name = "connectDataGridView";
+            this.connectDataGridView.Size = new System.Drawing.Size(345, 513);
+            this.connectDataGridView.TabIndex = 1;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Source
+            // 
+            this.Source.HeaderText = "Source";
+            this.Source.Name = "Source";
+            this.Source.ReadOnly = true;
+            // 
+            // Target
+            // 
+            this.Target.HeaderText = "Target";
+            this.Target.Name = "Target";
+            this.Target.ReadOnly = true;
+            // 
+            // termDataGridView
+            // 
+            this.termDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.termDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID2,
+            this.Term});
+            this.termDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.termDataGridView.Location = new System.Drawing.Point(0, 24);
+            this.termDataGridView.Name = "termDataGridView";
+            this.termDataGridView.Size = new System.Drawing.Size(345, 513);
+            this.termDataGridView.TabIndex = 2;
+            this.termDataGridView.Visible = false;
+            // 
+            // ID2
+            // 
+            this.ID2.HeaderText = "ID";
+            this.ID2.Name = "ID2";
+            this.ID2.ReadOnly = true;
+            // 
+            // Term
+            // 
+            this.Term.HeaderText = "Term";
+            this.Term.Name = "Term";
+            this.Term.ReadOnly = true;
+            // 
+            // sentanceDataGridView
+            // 
+            this.sentanceDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sentanceDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID3,
+            this.Veta,
+            this.Pojem});
+            this.sentanceDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sentanceDataGridView.Location = new System.Drawing.Point(0, 24);
+            this.sentanceDataGridView.Name = "sentanceDataGridView";
+            this.sentanceDataGridView.Size = new System.Drawing.Size(345, 513);
+            this.sentanceDataGridView.TabIndex = 3;
+            this.sentanceDataGridView.Visible = false;
+            // 
+            // ID3
+            // 
+            this.ID3.HeaderText = "ID";
+            this.ID3.Name = "ID3";
+            this.ID3.ReadOnly = true;
+            // 
+            // Veta
+            // 
+            this.Veta.HeaderText = "Veta";
+            this.Veta.Name = "Veta";
+            this.Veta.ReadOnly = true;
+            // 
+            // Pojem
+            // 
+            this.Pojem.HeaderText = "Pojem";
+            this.Pojem.Name = "Pojem";
+            this.Pojem.ReadOnly = true;
+            // 
+            // CTM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 537);
+            this.Controls.Add(this.sentanceDataGridView);
+            this.Controls.Add(this.termDataGridView);
+            this.Controls.Add(this.connectDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CTM";
+            this.Text = "CTM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.connectDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.termDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sentanceDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.DataGridView connectDataGridView;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem termsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sentancesToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Source;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Target;
+        private System.Windows.Forms.DataGridView termDataGridView;
+        private System.Windows.Forms.DataGridView sentanceDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Veta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pojem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Term;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class CTM : Form
+    {
+        public CTM()
+        {
+            InitializeComponent();
+        }
+        public void Compare(string sourceId, string CompareTerm)
+        {
+            int row = 0;
+            do
+            {
+                try
+                {
+                    if ((string.Compare(CompareTerm, termDataGridView[1, row].Value.ToString(), false)) == 0)
+                    {
+                        connectDataGridView.Rows.Add(
+                            (connectDataGridView.Rows.Count - 1),
+                            sourceId,
+                            termDataGridView[0, row].Value);
+                        return;
+                    }
+                    else
+                    {
+                        row++;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    MessageBox.Show("Term is not in the table:" + CompareTerm);
+                    return;
+                }
+            }
+            while (row < termDataGridView.Rows.Count);
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                               
+                            {
+                                connectDataGridView.Visible = true;
+                                sentanceDataGridView.Visible = false;
+                                termDataGridView.Visible = false;
+                                int row = 0;
+                                string line;
+                                while ((line = sr.ReadLine())!=null)
+                                {
+                                    string[]columns = line.Split('\t');
+                                    connectDataGridView.Rows.Add();
+                                    for (int i=0; i <columns.Length; i++)
+                                    {
+                                        connectDataGridView[i,row].Value=columns[i];
+                                    }
+                                    row++;
+                                }
+                        }
+
+                    }
+                }}
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Orighinal error:" + ex.Message);
+                }
+            }
+        }
+
+        private void termsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DataSet PojemDataSet = new DataSet();
+            
+            
+            
+          
+           
+           
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                               
+                            {
+                                connectDataGridView.Visible = false;
+                                sentanceDataGridView.Visible = false;
+                                termDataGridView.Visible = true;
+
+                                if (sentanceDataGridView.RowCount > 1)
+                                {
+                                    connectToolStripMenuItem.Enabled = true;
+                                }
+                                
+
+                                int row = 0;
+                                string line;
+                                while ((line = sr.ReadLine())!=null)
+                                {
+                                    string[]columns = line.Split('\t');
+                                    termDataGridView.Rows.Add();
+                                    for (int i=0; i <columns.Length; i++)
+                                    {
+                                        termDataGridView[i,row].Value=columns[i];
+                                    }
+                                    row++;
+                                }
+                        }
+
+                    }
+                }}
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Orighinal error:" + ex.Message);
+                    connectToolStripMenuItem.Enabled = false;
+                }
+            }
+        }
+
+        private void sentancesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DataSet PojemDataSet = new DataSet();
+
+
+
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FilterIndex == 1)
+                {
+                    try
+                    {
+                        if ((myStream = openFileDialog1.OpenFile()) != null)
+                        {
+                            using (myStream)
+                            {
+                                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                                {
+                                    connectDataGridView.Visible = false;
+                                    sentanceDataGridView.Visible = true;
+                                    termDataGridView.Visible = false;
+                                    if (termDataGridView.RowCount > 1)
+                                    {
+                                        connectToolStripMenuItem.Enabled = true;
+                                    }
+
+                                    int row = 0;
+                                    string line;
+                                    while ((line = sr.ReadLine()) != null)
+                                    {
+                                        string[] columns = line.Split('\t');
+                                        sentanceDataGridView.Rows.Add();
+                                        for (int i = 0; i < columns.Length; i++)
+                                        {
+                                            sentanceDataGridView[i, row].Value = columns[i];
+                                        }
+                                        row++;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Orighinal error:" + ex.Message);
+                        connectToolStripMenuItem.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            connectToolStripMenuItem.Enabled = false;
+            termDataGridView.Rows.Clear();
+            sentanceDataGridView.Rows.Clear();
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            connectDataGridView.Visible = true;
+            sentanceDataGridView.Visible = false;
+            termDataGridView.Visible = false;
+            
+            for (int i = 0; i<(sentanceDataGridView.Rows.Count - 1);i++)
+            {
+
+                if (sentanceDataGridView[2, i].Value.ToString() != string.Empty)
+                {
+                    string termCell = sentanceDataGridView[2, i].Value.ToString();
+
+                    if (termCell[0].ToString().Equals("\"".ToString()))
+                    {
+                        
+                        
+                        int j = 1;
+                        do
+                        {
+                            if (termCell[j].ToString().Equals(";".ToString()))
+                            {
+                                string trimCell = termCell.Substring(1, j-1);
+                                Compare(sentanceDataGridView[0, i].Value.ToString(), trimCell);
+                                termCell = termCell.Substring(j+1, termCell.Length - (j+1));
+                                j = 0;
+                            }
+                            j++;
+                        }
+                        while (j < termCell.Length);
+                        Compare(sentanceDataGridView[0, i].Value.ToString(), termCell.Substring(1, termCell.Length - 2));
+
+                    }
+                    else
+                    {
+                        if (termCell.Length > 0)
+                        {
+                            Compare(sentanceDataGridView[0, i].Value.ToString(), termCell);
+                        }
+                    }
+
+
+
+                }
+            }
+        
+          
+               
+             
+                
+                
+            }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text File|*.txt";
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+            StringBuilder builder = new StringBuilder();
+            int rowcount = connectDataGridView.Rows.Count;
+            int columncount = connectDataGridView.Columns.Count;
+
+            for (int i = 0; i < rowcount - 1; i++)
+            {
+                List<string> cols = new List<string>();
+                for (int j = 0; j < columncount; j++)
+                {
+                    cols.Add(connectDataGridView.Rows[i].Cells[j].Value.ToString());
+                }
+                builder.AppendLine(string.Join("\t", cols.ToArray()));
+            }
+            System.IO.File.WriteAllText(dialog.FileName, builder.ToString());
+            MessageBox.Show(@"Text file was created.");
+        }          
+            
+           }
+            
+
+
+        }
+    
+
